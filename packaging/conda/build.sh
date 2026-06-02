243b386c8da03d5d339135936b43534db9c4a079
Yasas1994 <yasas.wijesekara@gmail.com>
2026-06-02 22:25:04 +0200

    fix : added cargo-bundle-licenses

--- packaging/conda/build.sh
+++ packaging/conda/build.sh
@@ -1,16 +1,7 @@
-#!/bin/bash
-set -ex
-
-# Based on bioconda recipes for Rust packages:
-# https://github.com/bioconda/bioconda-recipes/tree/master/recipes/nanoq
-
-RUST_BACKTRACE=full
-
-if [ "$(uname)" == "Darwin" ]; then
-  # Ensure HOME is set for cargo on macOS
-  export HOME=$(pwd)
-fi
+#!/usr/bin/env bash
+set -euo pipefail
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
-cargo install -v --locked --root "$PREFIX" --path .
+# Install binary to PREFIX/bin
+cargo install --locked --root "$PREFIX" --path .
