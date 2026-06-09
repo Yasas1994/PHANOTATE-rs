c96e117a8438a12a90bd66a8fe0f02f3ebb22902
Yasas1994 <yasas.wijesekara@gmail.com>
2026-06-09 00:36:28 -0400

    chore: bumped version to 0.1.3

--- packaging/homebrew/phanotate-rs.rb
+++ packaging/homebrew/phanotate-rs.rb
@@ -1,7 +1,7 @@
 class PhanotateRs < Formula
   desc "Fast Rust implementation of PHANOTATE with automatic genetic-code detection"
   homepage "https://github.com/Yasas1994/PHANOTATE-rs"
-  url "https://github.com/Yasas1994/PHANOTATE-rs/archive/refs/tags/v0.1.2.tar.gz"
+  url "https://github.com/Yasas1994/PHANOTATE-rs/archive/refs/tags/v0.1.3.tar.gz"
   sha256 "PLACEHOLDER_SHA256"
   license "GPL-3.0"
 
